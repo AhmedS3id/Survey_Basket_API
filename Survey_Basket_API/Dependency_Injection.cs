ead966b09c582a10706f0bc396a5dc015b55da98
Ahmed Said <ahmeds3id711@gmail.com>
2026-06-16 18:43:01 +0300

    Media Type Api Version Reader

--- Survey_Basket_API/Dependency_Injection.cs
+++ Survey_Basket_API/Dependency_Injection.cs
@@ -135,7 +135,7 @@ namespace Survey_Basket_API
                 option.DefaultApiVersion = new ApiVersion(1, 0);
                 option.AssumeDefaultVersionWhenUnspecified = true;
                 option.ReportApiVersions = true;
-                option.ApiVersionReader = new QueryStringApiVersionReader("api-version");
+                option.ApiVersionReader = new MediaTypeApiVersionReader("api-version");
                // option.ApiVersionReader = new HeaderApiVersionReader("x-api-version");
             }).AddApiExplorer(option =>
             {
