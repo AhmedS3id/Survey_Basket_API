e0778a82244fc23720ab7268c266236d057c9748
Ahmed Said <ahmeds3id711@gmail.com>
2026-06-16 18:44:42 +0300

    Final shape of section api versions

--- Survey_Basket_API/Dependency_Injection.cs
+++ Survey_Basket_API/Dependency_Injection.cs
@@ -135,8 +135,7 @@ namespace Survey_Basket_API
                 option.DefaultApiVersion = new ApiVersion(1, 0);
                 option.AssumeDefaultVersionWhenUnspecified = true;
                 option.ReportApiVersions = true;
-                option.ApiVersionReader = new MediaTypeApiVersionReader("api-version");
-               // option.ApiVersionReader = new HeaderApiVersionReader("x-api-version");
+                option.ApiVersionReader = new HeaderApiVersionReader("x-api-version");
             }).AddApiExplorer(option =>
             {
                 option.GroupNameFormat = "'v'V";
